131ca87c9dd480075940b834179ac75ea49dcb39
Prachi Tripathi <114851153+tripathiprachi18@users.noreply.github.com>
2023-02-21 23:46:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,56 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+mport React, { useEffect } from 'react';
+import Header from "./Header"
+import Sidebar from './Sidebar';
+import Feed from './Feed'
+import Widget from'./Widget'
+import Login from "./Login"
+import { useSelector,useDispatch } from 'react-redux';
+import {selectUser,loginuser} from './features/userSlice';
+import { auth } from './firebase';
 
+const App =()=> {
+  const user=useSelector(selectUser);
+  const dispatch=useDispatch();
+  //jaise hi hamri app hoti h render then hamra useEffect chalega
+  useEffect(()=>{
+    const checkUser = () => {
+      auth.onAuthStateChanged((userAuth)=>{
+        if(userAuth){
+          //already login
+          dispatch(loginuser({
+            email:userAuth.email,
+            uid:userAuth.uid,
+            photoURL:userAuth.photoURL,
+            displayName:userAuth.displayName
+          }))
+  
+  
+        }
+      })
+    }
+    checkUser()
+
+  },[dispatch])
+
+  
 
   return (
-    <div id="main">
+    <>
+    {
+      !user ? (<Login/>) : (
+    <div className='app_wrapper'>
+        <Header/>
+      <div className='app_body'>
+        <Sidebar/>
+        <Feed/>
+        <Widget/>
+      </div>
     </div>
+      )
+    }
+    </>
+   
   )
 }
-
 
 export default App;
