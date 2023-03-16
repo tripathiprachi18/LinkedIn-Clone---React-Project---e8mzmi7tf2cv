6620fb662673d0f9a7dec345d00daeab0c88180f
Prachi Tripathi <114851153+tripathiprachi18@users.noreply.github.com>
2023-03-16 22:41:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAq-tbhCV_YDT6E1LY_I-fk5mPr3Eui1SE",
-    authDomain: "linkedinclone-6a9b3.firebaseapp.com",
-    projectId: "linkedinclone-6a9b3",
-    storageBucket: "linkedinclone-6a9b3.appspot.com",
-    messagingSenderId: "920270243031",
-    appId: "1:920270243031:web:1ff7700a977e418ed89c92"
-  };
+  apiKey: "AIzaSyAD8X0UP7lujzD4DNHKIHID57lYt3lmuYg",
+  authDomain: "linkedin-clone-950c5.firebaseapp.com",
+  projectId: "linkedin-clone-950c5",
+  storageBucket: "linkedin-clone-950c5.appspot.com",
+  messagingSenderId: "727659279025",
+  appId: "1:727659279025:web:5f59f45d9a0db0a321e890",
+  measurementId: "G-6X2QC40WNK"
+};
   //this special line of code here connects everything
   const firebaseApp=firebase.initializeApp(firebaseConfig);
   //get the firebase databse
